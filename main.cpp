60511e7e92994fbeab3de7689608bfbe7a5fdba0
astricka1337 <algirdasstricka@gmail.com>
2026-05-25 10:25:01 +0300

    Idetas meniu nuskaitymas is failo ir parodymas ekrane

--- main.cpp
+++ main.cpp
@@ -5,13 +5,11 @@
 
 using namespace std;
 
-// Užduoties struktūra
 struct menuItemType {
     string menuItem;
     double menuPrice;
 };
 
-// Tik prototipai
 void getData(menuItemType menuList[], int& count);
 void showMenu(const menuItemType menuList[], int count);
 void printCheck(const menuItemType menuList[], int count, const int orderQuantities[]);
@@ -23,11 +21,51 @@ int main() {
     int n = 0;
     int kiekiai[100] = {0};
 
-    cout << "Programa pradėta..." << endl;
+    // Jau kviečiame nuskaitymą ir rodymą
+    getData(menuList, n);
+    if (n == 0) {
+        cout << "Klaida: Nerastas meniu!" << endl;
+        return 1;
+    }
+
+    showMenu(menuList, n);
     return 0;
 }
 
-// Tuščios funkcijos (paruoštos ateičiai)
-void getData(menuItemType menuList[], int& count) {}
-void showMenu(const menuItemType menuList[], int count) {}
+// Įrašom logiką failo nuskaitymui
+void getData(menuItemType menuList[], int& count) {
+    ifstream failas("menu.txt");
+    if (!failas.is_open()) return;
+
+    string eilute;
+    count = 0;
+    while (getline(failas, eilute) && count < 100) {
+        if (eilute.empty()) continue;
+        int tarpas = -1;
+        for (int i = eilute.length() - 1; i >= 0; i--) {
+            if (eilute[i] == ' ') {
+                tarpas = i;
+                break;
+            }
+        }
+        if (tarpas != -1) {
+            menuList[count].menuItem = eilute.substr(0, tarpas);
+            menuList[count].menuPrice = stod(eilute.substr(tarpas + 1));
+            count++;
+        }
+    }
+    failas.close();
+}
+
+// Įrašom logiką meniu rodymui
+void showMenu(const menuItemType menuList[], int count) {
+    cout << "=== Pusryčių Meniu ===" << endl;
+    for (int i = 0; i < count; i++) {
+        cout << i + 1 << ". "
+             << left << setw(38) << menuList[i].menuItem
+             << right << setw(6) << fixed << setprecision(2) << menuList[i].menuPrice << "€" << endl;
+    }
+    cout << "======================" << endl;
+}
+
 void printCheck(const menuItemType menuList[], int count, const int orderQuantities[]) {}
